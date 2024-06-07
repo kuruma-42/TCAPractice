beababfa0bea8c0e6541846e6a98b8c9c08ed3fd
ChaCha <perfect_yon@naver.com>
2024-06-07 17:08:14 +0900

    study: add first presentation

---
+++ My/AddContactView.swift
@@ -0,0 +1,8 @@
+//
+//  AddContactView.swift
+//  My
+//
+//  Created by IK-LPC-084 on 6/7/24.
+//
+
+import Foundation
---
+++ My/ContactFeature.swift
@@ -0,0 +1,8 @@
+//
+//  ContactFeature.swift
+//  My
+//
+//  Created by IK-LPC-084 on 6/7/24.
+//
+
+import Foundation
